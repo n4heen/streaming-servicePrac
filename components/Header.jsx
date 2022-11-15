ecfbe09292781387198f5558caca3ffa500b5f0d
Naheen Habib <naheenhabib@outlook.com>
2022-11-15 19:05:36 +1100

    new commit

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,12 +1,26 @@
 import Image from 'next/image'
 import React from 'react'
 import HeaderItem from './HeaderItem'
+import{
+    CheckBadgeIcon,
+    CircleStackIcon,
+    HomeIcon,
+    BoltIcon,
+    MagnifyingGlassIcon,
+    UserIcon
+    
+} from "@heroicons/react/24/outline"
 
 export default function Header() {
     return (
-        <header className=''>
-            <div>
-                <HeaderItem />
+        <header className='flex flex-col sm:flex-row m-5 justify-between items-center h-auto'>
+            <div className='flex flex-grow justify-evenly max-w-2xl'>
+                <HeaderItem title='HOME' Icon={HomeIcon} />
+                <HeaderItem title='TRENDING' Icon={BoltIcon} />
+                <HeaderItem title='VERIFIED' Icon={CheckBadgeIcon} />
+                <HeaderItem title='COLLECTIONS' Icon={CircleStackIcon} />
+                <HeaderItem title='SEARCH' Icon={MagnifyingGlassIcon} />
+                <HeaderItem title='ACCOUNT' Icon={UserIcon} />
             </div>
             <Image
                 className='object-contain'
