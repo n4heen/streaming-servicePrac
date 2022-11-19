5747c304dfd4de83282ef69e0a41cc5065db99f9
Naheen Habib <naheenhabib@outlook.com>
2022-11-19 21:21:51 +1100

    newcommit

--- components/Results.jsx
+++ components/Results.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import Thumbnail from './Thumbnail'
 
-export default function Results() {
-  return (
-    <div>Results</div>
-  )
+export default function Results({ requests }) {
+    return (
+        <div>
+            {requests.map(result => (
+                <Thumbnail key={result.id} result={result} />
+            ))}
+        </div>
+    )
 }
---
+++ components/Thumbnail.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Thumbnail() {
+  return (
+    <div>Thumbnail</div>
+  )
+}
