80e18576eab8231706e95efb9dfd36812e37b781
Naheen Habib <naheenhabib@outlook.com>
2022-11-19 20:30:01 +1100

    new commit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Header from '../components/Header'
 import Nav from '../components/Nav'
+import Results from '../components/Results'
 
 
 
@@ -16,8 +17,8 @@ export default function Home() {
 
       <Header />
       <Nav />
-    
-    
+      <Results />
+
     </div>
   )
 }
