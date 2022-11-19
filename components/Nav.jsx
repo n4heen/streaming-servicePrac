85e578fb336061adfee832e4118b0cafc05e5af5
Naheen Habib <naheenhabib@outlook.com>
2022-11-19 20:05:03 +1100

    new commit

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -4,8 +4,10 @@ import requests from '../utils/requests'
 export default function Nav() {
   return (
     <nav>
-      <div>
-        {Object.entries(requests).map(([key, { title, url }]))}
+      <div className=''>
+        {Object.entries(requests).map(([key, { title, url }]) => (
+          <h2 key={key}>{title}</h2>
+        ))}
       </div>
     </nav>
   )
